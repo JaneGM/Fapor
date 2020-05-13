4411ce10036e8a1eeeeff18639009230391dfe63
JaneGM <janegm61@gmail.com>
2020-05-13 12:05:43 +0300

    переделано в SCSS

--- js/scripts.js
+++ js/scripts.js
@@ -22,26 +22,6 @@ $( function() {
     $( "#accordion" ).accordion();
 } );
 
-$( function() {
-    $( "#dialog" ).dialog({
-        autoOpen: false,
-        show: {
-            effect: "blind",
-            duration: 1000
-        },
-        hide: {
-            effect: "explode",
-            duration: 1000
-        }
-    });
-
-    $( "#opener" ).on( "click", function() {
-        $( "#dialog" ).dialog( "open" );
-    });
-} );
-
-
-
 let navigation = {
     // Variables
     $navTrigger: document.querySelector('.nav__trigger'),
